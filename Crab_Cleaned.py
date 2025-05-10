cf7a20013835112cb2925d949df991fd3b621c57
ashteronii <ashtyn.gagner@icloud.com>
2025-05-10 17:11:34 -0500

    Updated comments.

--- Crab_Cleaned.py
+++ Crab_Cleaned.py
@@ -6,11 +6,11 @@ crabs = pd.read_csv("CrabAgePrediction.csv")
 
 # Cleans and preprocesses the crab dataset.
 # Steps:
-#    - Drops missing values.
-#    - Removes duplicate rows.
-#    - Removes invalid height values.
-#    - Filters out implausible dimensions.
-#    - One-hot encodes the 'Sex' column.
+#    - Drop missing values.
+#    - Remove duplicate rows.
+#    - Remove invalid height values.
+#    - Filter out implausible dimensions.
+#    - One-hot encode the 'Sex' column.
 # This ensures that we only work with complete and unique records for analysis.
 crabs = crabs.dropna().drop_duplicates()
 crabs = crabs.query("Height != 0")
